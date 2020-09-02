1ac431a6ec3e71c589b51bb36292af65c4267e2b
Juan Pablo <jpdev@ckcomercializadora.com>
2020-09-02 11:33:43 -0500

    Se le agrega nombres diferentes al sidebar para las funcionalidades de carga de archivos

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -38,8 +38,8 @@
 			@endif
 			
 			@if (Auth::user()->roles_id == 1 || Auth::user()->roles_id == 5)
-				<li><a href="{{url('planos')}}"><i class="fa fa-file-text-o"></i> <span>Carga de Archivos</span></a></li>			
-				<li><a href="{{url('planos/crear_gcp')}}"><i class="fa fa-file-text-o"></i> <span>Carga de Archivos V2</span></a></li>			
+				<li><a href="{{url('planos')}}"><i class="fa fa-file-text-o"></i> <span>Carga de Archivos Manual</span></a></li>			
+				<li><a href="{{url('planos/crear_gcp')}}"><i class="fa fa-file-text-o"></i> <span>Carga de Archivos Inteligente</span></a></li>			
 				{{-- <li><a href="{{url('comerciales')}}"><i class="fa fa-user"></i> <span>Comerciales</span></a></li> --}}
 				{{-- <li><a href="{{url('terecuperamos')}}"><i class="fa fa-handshake-o"></i> <span>T-Recuperamos</span></a></li> --}}
 				{{-- <li><a href="{{url('reportes')}}"><i class="fa fa-bar-chart-o"></i> <span>Reportes</span></a></li> --}}
