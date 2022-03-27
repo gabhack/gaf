c6b5989a00c323094f0a1494684fb6c9d9b5d447
IvanNicolas <nickiepierre@gmail.com>
2022-03-27 10:17:53 -0500

    Migraciones Ivan

--- database/migrations/defaultDB/2022_03_26_185036_descapli.php
+++ database/migrations/defaultDB/2022_03_26_185036_descapli.php
@@ -13,8 +13,32 @@ class Descapli extends Migration
      */
     public function up()
     {
+
         Schema::connection('pgsql')->create('descalpi', function($table){
             $table->increments('id');
+            $table->string('periodo');
+            $table->string('concecutivo');
+            $table->string('clase');
+            $table->string('tercero');
+            $table->string('nomtercero');
+            $table->string('td');
+            $table->string('doc');
+            $table->string('nomp');
+            $table->string('pagare');
+            $table->string('porcentaje');
+            $table->string('vaplicado');
+            $table->string('vtotal');
+            $table->string('vpagado');
+            $table->string('saldo');
+            $table->string('fgrab');
+            $table->string('forma');
+            $table->string('codentiant');
+            $table->string('nonentant');
+            $table->string('fechacesion');
+            $table->string('tdesc');
+            $table->string('p5d');
+            $table->string('p4d');
+            $table->string('numpagopt');
         });
     }
 
--- database/migrations/defaultDB/2022_03_26_185052_descnoap.php
+++ database/migrations/defaultDB/2022_03_26_185052_descnoap.php
@@ -11,10 +11,31 @@ class Descnoap extends Migration
      *
      * @return void
      */
+
     public function up()
     {
         Schema::connection('pgsql')->create('descnoap', function($table){
             $table->increments('id');
+            $table->string('clase');
+            $table->string('tercero');
+            $table->string('nomtercero');
+            $table->string('td');
+            $table->string('doc');
+            $table->string('nomp');
+            $table->string('pagare');
+            $table->string('porcentaje');
+            $table->string('vfijo');
+            $table->string('vaplicado');
+            $table->string('vtotal');
+            $table->string('vpagado');
+            $table->string('saldo');
+            $table->string('fgrab');
+            $table->string('forma');
+            $table->string('incon');
+            $table->string('codentiant');
+            $table->string('nonentant');
+            $table->string('fechacesion');
+            $table->string('tdesc');
         });
     }
 
