fb2c8a142aee32b2de8e4b1b31d995ed4aa4f6c7
Brayanstyd <brayantriana22@gmail.com>
2022-03-27 20:18:59 -0500

    Configuración de datatables para consulta de creditos de cliente

--- resources/assets/js/components/pages/ConsultDataClient/index.vue
+++ resources/assets/js/components/pages/ConsultDataClient/index.vue
@@ -2,31 +2,231 @@
     <div class="container-fluid">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="menu1-tab" data-toggle="tab" href="#menu1" role="tab" aria-controls="menu1" aria-selected="true">Menu 1</a>
+                <a class="nav-link active" id="menu1-tab" data-toggle="tab" href="#menu1" role="tab" aria-controls="menu1" aria-selected="true">Fecha Vinculación</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="menu2-tab" data-toggle="tab" href="#menu2" role="tab" aria-controls="menu2" aria-selected="false">Menu 2</a>
+                <a class="nav-link" id="menu2-tab" data-toggle="tab" href="#menu2" role="tab" aria-controls="menu2" aria-selected="false">Data Mes</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="menu3-tab" data-toggle="tab" href="#menu3" role="tab" aria-controls="menu3" aria-selected="false">Menu 3</a>
+                <a class="nav-link" id="menu3-tab" data-toggle="tab" href="#menu3" role="tab" aria-controls="menu3" aria-selected="false">Descapli</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="menu4-tab" data-toggle="tab" href="#menu4" role="tab" aria-controls="menu4" aria-selected="false">Menu 4</a>
+                <a class="nav-link" id="menu4-tab" data-toggle="tab" href="#menu4" role="tab" aria-controls="menu4" aria-selected="false">Descanoap</a>
+            </li>
+
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="menu5-tab" data-toggle="tab" href="#menu5" role="tab" aria-controls="menu5" aria-selected="false">Resultado</a>
             </li>
         </ul>
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="menu1" role="tabpanel" aria-labelledby="menu1-tab">
-                Menu 1
+                <table class="table table-responsive table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Documento</th>
+                            <th>Vinculación</th>
+                            <th>Tipo Pensión</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1026303251</td>
+                            <td>13095b33b23</td>
+                            <td>Vinculacion</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             
             <div class="tab-pane fade" id="menu2" role="tabpanel" aria-labelledby="menu2-tab">
-                Menu 2
+                <table class="table table-responsive table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Fondo</th>
+                            <th>Tipo de Documento</th>
+                            <th>x</th>
+                            <th>Pensionado</th>
+                            <th>Fecha de Nacimiento</th>
+                            <th>Dirección</th>
+                            <th>Departamento</th>
+                            <th>Municipio</th>
+                            <th>Tipo Pensión</th>
+                            <th>Nombre Banco</th>
+                            <th>Sucursal</th>
+                            <th>Telefono</th>
+                            <th>Celular</th>
+                            <th>Correo</th>
+                            <th>Valor Pensiones</th>
+                            <th>Valor Salud</th>
+                            <th>Valor Embargos</th>
+                            <th>Valor Descuento</th>
+                            <th>Cupo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1026303251</td>
+                            <td>13095b33b23</td>
+                            <td>Vinculacion</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="tab-pane fade" id="menu3" role="tabpanel" aria-labelledby="menu3-tab">
-                Menu 3
+                <table class="table table-responsive table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Periodo</th>
+                            <th>Consecutivo</th>
+                            <th>Clase Tercero</th>
+                            <th>Tercero</th>
+                            <th>Nombre del Tercero</th>
+                            <th>Tipo de Documento</th>
+                            <th>Documento</th>
+                            <th>Nombre</th>
+                            <th>Pagare</th>
+                            <th>Porcentaje</th>
+                            <th>Valor Aplicado</th>
+                            <th>Valor Total</th>
+                            <th>Valor Pagado</th>
+                            <th>Saldo</th>
+                            <th>Fecha Grabación</th>
+                            <th>Forma</th>
+                            <th>Codigo Entidad Anterior</th>
+                            <th>Nombre Entidad Anterior</th>
+                            <th>Fecha de Cesión</th>
+                            <th>Tipo Descuento</th>
+                            <th>Pagare 5 Dig</th>
+                            <th>PAGARE4DIGCON0</th>
+                            <th>Numero pagare</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1026303251</td>
+                            <td>13095b33b23</td>
+                            <td>Vinculacion</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>
+                                <input type="checkbox"/>
+                                1026303251
+                            </td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>1026303251</td>
+                            <td>13095b33b23</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="tab-pane fade" id="menu4" role="tabpanel" aria-labelledby="menu4-tab">
-                Menu 4
+                <table class="table table-responsive table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Clase Tercero</th>
+                            <th>Tercero</th>
+                            <th>Nombre Tercero</th>
+                            <th>Tipo de Documento</th>
+                            <th>Documento</th>
+                            <th>Nombre</th>
+                            <th>Pagare</th>
+                            <th>Porcentaje</th>
+                            <th>Valor Fijo</th>
+                            <th>Valor Aplicado</th>
+                            <th>Valor Total</th>
+                            <th>Valor Pagad0</th>
+                            <th>Saldo</th>
+                            <th>Fecha Grabación</th>
+                            <th>Forma</th>
+                            <th>Inconsistencia</th>
+                            <th>Código entidad anterior</th>
+                            <th>Nombre Entidad Anterior</th>
+                            <th>Fecha de Cesión</th>
+                            <th>Tipo Descuento</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1026303251</td>
+                            <td>13095b33b23</td>
+                            <td>Vinculacion</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>
+                                <input type="checkbox"/>13095b33b23                                
+                            </td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                            <td>13095b33b23</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="tab-pane fade" id="menu5" role="tabpanel" aria-labelledby="menu5-tab">
+                <table class="table table-responsive table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Aprobado</th>
+                            <th>% de incorporacion</th>
+                            <th>Cuota Maxima de incorporacion</th>
+                            <th>Fecha resp.</th>
+                            <th>Fecha Vinculacion</th>
+                            <th>Tipo Vinculacion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Si</td>
+                            <td>2</td>
+                            <td>40</td>
+                            <td>10-10-2020</td>
+                            <td>11-11-2021</td>
+                            <td>Directa</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -38,10 +238,41 @@ export default {
             tabView:1
         }
     },
+    mounted(){
+        
+    },
     methods:{
-        changeViewMenu(data){
-            this.tabView = data;
-        }
+        getFechaVinc(){
+            axios.get('').then((response)=>{
+
+            }).catch((error)=>{
+                console.log(error);
+            })
+        },
+
+        getDataMes(){
+            axios.get('').then((response)=>{
+
+            }).catch((error)=>{
+                console.log(error);
+            })
+        },
+
+        getDescapli(){
+            axios.get('').then((response)=>{
+
+            }).catch((error)=>{
+                console.log(error);
+            })
+        },
+
+        getDescnoap(){
+            axios.get('').then((response)=>{
+
+            }).catch((error)=>{
+                console.log(error);
+            })
+        },
     }
 }
 </script>
