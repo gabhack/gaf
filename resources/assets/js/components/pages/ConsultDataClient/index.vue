fcbe48ab08aadc1a2a3a312d99e2c1e2cec96d1f
Brayanstyd <brayantriana22@gmail.com>
2022-05-19 17:06:44 -0500

    Configuración de visualizacion de menú

--- resources/assets/js/components/pages/ConsultDataClient/index.vue
+++ resources/assets/js/components/pages/ConsultDataClient/index.vue
@@ -25,22 +25,22 @@
                         <div class="row">
                             <div class="col-6">
                                 <b class="panel-label">CEDULA:</b>                                
-                                <input class="form-control text-center" type="number" v-model="dataclient.doc">
+                                <input required class="form-control text-center" type="number" v-model="dataclient.doc">
                             </div>
                             <div class="col-6">
-                                <b class="panel-label">NOMBRE:</b>                                
-                                <input class="form-control text-center" type="text" v-model="dataclient.name">
+                                <b class="panel-label">NOMBRES Y APELLIDOS:</b>                                
+                                <input required class="form-control text-center" type="text" v-model="dataclient.name">
                             </div>
                             <div class="col-6">
                                 <b class="panel-label">PAGADURIA:</b>
-                                <select class="form-control" v-model="dataclient.pagaduria">
+                                <select required class="form-control" v-model="dataclient.pagaduria">
                                     <option value="FOPEP">FOPEP</option>
                                     <option value="FIDUPREVISORA">FIDUPREVISORA</option>
                                     <option value="FODE VALLE">FODE VALLE</option>                                        
                                 </select>                                    
                             </div>                                
                             <div class="col-6 mt-4">
-                            <button type="button" class="btn btn-primary" v-on:click="getData">CONSULTAR</button>
+                            <button type="button" v-if="dataclient.pagaduria && dataclient.name && dataclient.doc" class="btn btn-primary" v-on:click="getData">CONSULTAR</button>
                         </div>                            
                     </div>
                 </div>
