6a266feed15469060795fc7478c5352a08c11641
Jhon Michael Garcia <me@maikoldev.com>
2022-09-21 11:42:15 -0500

    Se añade información de Score en vista Consulta Cifin.
    - Se añade boton de descargar pdf, se crea componente global de vue.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -50,6 +50,7 @@ Vue.component('AmiPersonas', require('./components/pages/AmiPersonas').default);
 Vue.component('MoreInformation', require('./components/pages/MoreInformation').default);
 Vue.component('Hego', require('./components/pages/Hego').default);
 Vue.component('HegoInformation', require('./components/pages/HegoInformation').default);
+Vue.component('download-pdf-button', require('./components/DownloadPdfButton').default);
 
 // Pages
 Vue.component('ConsultasIndex', require('./components/pages/Consultas/Index').default);
