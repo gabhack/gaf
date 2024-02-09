22dcb2fd3f7a6977001431fca32a5b39b7bfae25
ingJuanMolano <ing.juanmolano@gmail.com>
2024-02-09 11:15:00 -0500

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.applogin')
 
 @section('content')
-<div class="row h-100">
-  <div class="col-md-6 d-none d-md-flex align-items-end">
-    <img src="/img/amipersonas1.svg" class="img-fluid pb-3 w-100" />
+<div class="row h-100 img-background">
+  <div class="col-xl-5 d-none d-xl-flex align-items-center justify-content-end">
+    <img src="/img/INGRESO-IMG-GAF2.png" class="img-fluid w-75" />
   </div>
-  <div class="col-xs-10 col-md-6 d-flex align-items-center justify-content-center">
+  <div class="col-xs-10 col-xl-7 d-flex align-items-center justify-content-center">
     <div class="text-center">
       <h1 class="title font-weight-bold text-spring-green mb-0">
         GAF
       </h1>
       <p class="text font-weight-bold mb-4">
-        <span class="text-spring-green">AMI</span>
-        - HEGO
+        <!-- <span class="text-spring-green">AMI</span> -->
+        AMI - HEGO
         <!-- <span class="text-spring-green">Inteligente</span> -->
       </p>
-      <div class="w-50 mx-auto text-center">
+      <div>
         <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
 
@@ -38,23 +38,25 @@
               </span>
             @endif
           </div>
-
-          <button type="submit" class="btn btn-outline-black-pearl btn-submit">
-            Ingresar
-          </button>
-
-          <div class="mt-3">
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" id="remember" name="remember" class="custom-control-input" {{ old('remember') ? 'checked' : '' }}>
-              <label class="custom-control-label" for="remember">Recuérdame</label>
+          <div class="mt-3 pb-5">
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" id="remember" name="remember" class="custom-control-input" {{ old('remember') ? 'checked' : '' }}>
+                <label class="custom-control-label password" for="remember">Recuérdame</label>
+              </div>
+              <a class="text-black-pearl password" href="{{ route('password.request') }}">
+                ¿Olvidaste la contraseña?
+              </a>
             </div>
-            <a class="text-black-pearl" href="{{ route('password.request') }}">
-              ¿Olvidaste la contraseña?
-            </a>
-          </div>
-
+          <div class="align-items-center justify-content-center" style="display: grid;">
+            <button type="submit" class="btn-submit">
+              Ingresar al Portal Cliente
+            </button>
+            <!-- <button  class="btn-submit1">
+              Crear nuevo usuario
+            </button> -->
+        </div>
           <div class="mt-3">
-            <p class="text-spring-green font-weight-light">Powered by <span class="font-weight-bold">GAF Solutions™</span></p>
+            <p class="text-spring-green font-weight-light power">Powered by <span class="font-weight-bold">GAF Solutions™</span></p>
           </div>
         </form>
       </div>
