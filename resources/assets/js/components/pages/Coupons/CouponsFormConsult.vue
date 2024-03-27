e4640ac0ba4365657482908e1912c666bd4751ca
spotfinder-dev <pbl.mendez@gmail.com>
2024-03-27 17:41:17 -0500

    buscador y moras

--- resources/assets/js/components/pages/Coupons/CouponsFormConsult.vue
+++ resources/assets/js/components/pages/Coupons/CouponsFormConsult.vue
@@ -142,34 +142,53 @@
                 <b>RESULTADOS DE LA CONSULTA (Cartera en Mora)</b>
             </div>
             <div class="panel-body">
+                <b-form-input
+                    v-model="filtroDescuento"
+                    placeholder="Buscar por documento..."
+                    class="mb-3"
+                ></b-form-input>
                 <div class="table-responsive">
-                    <b-table striped id="mora-table" hover :fields="descuentosFields" :items="paginatedDescuentos">
+                    <b-table striped id="mora-table" hover :fields="descuentosFields" :items="descuentosFiltrados">
                         <template v-slot:cell(actions)="{ item }">
-                            <b-button v-b-modal.modal-1 variant="primary" @click="handleButtonClick(item.doc)">
+                            <b-button v-b-modal.modal-1 variant="primary" @click="handleButtonClick(item.doc, item.id)">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25">
                                     <g id="_01_align_center" data-name="01 align center">
-                                        <path d="M23.821,11.181v0C22.943,9.261,19.5,3,12,3S1.057,9.261.179,11.181a1.969,1.969,0,0,0,0,1.64C1.057,14.739,4.5,21,12,21s10.943-6.261,11.821-8.181A1.968,1.968,0,0,0,23.821,11.181ZM12,19c-6.307,0-9.25-5.366-10-6.989C2.75,10.366,5.693,5,12,5c6.292,0,9.236,5.343,10,7C21.236,13.657,18.292,19,12,19Z"/>
-                                        <path d="M12,7a5,5,0,1,0,5,5A5.006,5.006,0,0,0,12,7Zm0,8a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"/>
+                                        <path
+                                            d="M23.821,11.181v0C22.943,9.261,19.5,3,12,3S1.057,9.261.179,11.181a1.969,1.969,0,0,0,0,1.64C1.057,14.739,4.5,21,12,21s10.943-6.261,11.821-8.181A1.968,1.968,0,0,0,23.821,11.181ZM12,19c-6.307,0-9.25-5.366-10-6.989C2.75,10.366,5.693,5,12,5c6.292,0,9.236,5.343,10,7C21.236,13.657,18.292,19,12,19Z"
+                                        />
+                                        <path
+                                            d="M12,7a5,5,0,1,0,5,5A5.006,5.006,0,0,0,12,7Zm0,8a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"
+                                        />
                                     </g>
                                 </svg>
                             </b-button>
                         </template>
                     </b-table>
                     <b-modal id="modal-1" centered title="Causales" @hidden="clearCausales">
-                        <ul>
-                            <h5 v-if="this.causalesFinal.length === 0">No Hay Causales en los Datos Actuales</h5>
-                            <h5 v-if="this.causalesFinal.length > 0">Embargado Por:</h5>
-                            <li v-for="causalT in causalesFinal">
-                                {{ causalT.entidad }} - {{ causalT.docentidad }} - ${{ causalT.valor }}
-                            </li>
-                        </ul>
-                        <template #modal-footer="{hide}">
-                            
-                            <b-button size="md" variant="outline-secondary" @click="hide('forget')">
-                                CERRAR
-                            </b-button>
+                        <div v-if="causalesFinal.some(c => c.motivo === 'Embargo')">
+                            <h5>Embargado Por:</h5>
+                            <ul>
+                                <li v-for="causal in causalesFinal.filter(c => c.motivo === 'Embargo')">
+                                    {{ causal.entidad }} - {{ causal.docentidad }} - ${{ causal.valor }}
+                                </li>
+                            </ul>
+                        </div>
+                        <div v-if="causalesFinal.some(c => c.motivo === 'Mora')">
+                            <h5>En Mora Con:</h5>
+                            <ul>
+                                <li v-for="causal in causalesFinal.filter(c => c.motivo === 'Mora')">
+                                    {{ causal.entidad }} - ${{ causal.valor }}
+                                </li>
+                            </ul>
+                        </div>
+                        <div v-if="causalesFinal.length === 0">
+                            <h5>No Hay Causales en los Datos Actuales</h5>
+                        </div>
+                        <template #modal-footer="{ hide }">
+                            <b-button size="md" variant="outline-secondary" @click="hide('forget')">CERRAR</b-button>
                         </template>
-                       </b-modal>
+                    </b-modal>
+
                     <b-pagination
                         v-model="currentPageMora"
                         :per-page="perPageMora"
@@ -593,7 +612,8 @@ export default {
             totalClientes: 0,
             totalCuotas: 0,
             causales: [],
-            causalesFinal: []
+            causalesFinal: [],
+            filtroDescuento: ''
         };
     },
     watch: {
@@ -620,6 +640,14 @@ export default {
                 ...item,
                 egresos: this.formatCurrency(item.egresos)
             }));
+        },
+        descuentosFiltrados() {
+            if (!this.filtroDescuento) {
+                return this.paginatedDescuentos;
+            }
+            return this.paginatedDescuentos.filter(descuento =>
+                descuento.doc.toLowerCase().includes(this.filtroDescuento.toLowerCase())
+            );
         },
         paginatedDescuentos() {
             const start = (this.currentPageMora - 1) * this.perPageMora;
@@ -700,8 +728,10 @@ export default {
                 this.descuentos = descuentosResponse.data;
                 this.embargos = embargosResponse.data;
                 //LLenar CAUSALES
+                //con embargos
                 this.embargos.forEach(embargo => {
                     const causal = {
+                        id: embargo.id,
                         motivo: 'Embargo',
                         entidad: embargo.entidaddeman,
                         docentidad: embargo.docdeman,
@@ -710,7 +740,19 @@ export default {
                     };
                     this.causales.push(causal);
                 });
-                console.log(this.causales);
+                //con descuentos
+                this.descuentos.forEach(descuento => {
+                    const causal = {
+                        id: descuento.id,
+                        motivo: 'Mora',
+                        entidad: descuento.mliquid,
+                        docentidad: '',
+                        doc: descuento.doc,
+                        valor: descuento.valor
+                    };
+                    this.causales.push(causal);
+                });
+
                 // Calcula los totales y cuenta los elementos para cada tipo
                 this.rowsAldia = this.coupons.length;
                 this.rowsMora = this.descuentos.length;
@@ -752,30 +794,30 @@ export default {
                 const parsed = parseFloat(value);
                 return isNaN(parsed) ? 0 : parsed;
             };
+            causalesRelacionados;
             return items.reduce((total, item) => total + parseToNumber(item[key]), 0);
         },
-        getCausalesByDoc(doc) {
-            return this.causales.filter(causal => causal.doc === doc);
+        getCausalesByDoc(doc, idRow) {
+            console.log(this.causales);
+            console.log(idRow);
+            // Filtrado más complejo para obtener los causales relacionados
+            return this.causales.filter(
+                causal =>
+                    (causal.motivo === 'Embargo' && causal.doc === doc) ||
+                    (causal.motivo === 'Mora' && causal.doc === doc && causal.id != idRow)
+            );
         },
-
-        handleButtonClick(doc) {
-            const causalesRelacionados = this.getCausalesByDoc(doc);
-
-            
+        handleButtonClick(doc, idRow) {
+            const causalesRelacionados = this.getCausalesByDoc(doc, idRow);
             causalesRelacionados.forEach(causalEmbargo => {
-                    const cuasalesDeEmbargo = {
-                        entidad: causalEmbargo.entidad,
-                        docentidad: causalEmbargo.docentidad,
-                        valor: causalEmbargo.valor
-                    };
-                    this.causalesFinal.push(cuasalesDeEmbargo);
-                    console.log(this.causalesFinal);
-                });
-                
-
-            console.log(causalesRelacionados);
+                const cuasalesDeEmbargo = {
+                    entidad: causalEmbargo.entidad,
+                    docentidad: causalEmbargo.docentidad,
+                    valor: causalEmbargo.valor
+                };
+                this.causalesFinal.push(cuasalesDeEmbargo);
+            });
         }
-
     }
 };
 </script>
