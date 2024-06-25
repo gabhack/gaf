60b343c042a0d76f7efcdb17dd29ecc54ac6cb77
spotfinder-dev <pbl.mendez@gmail.com>
2024-06-25 09:16:07 -0500

    menu con join

--- resources/views/ami.blade.php
+++ resources/views/ami.blade.php
@@ -15,7 +15,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-12 d-flex justify-content-center align-items-center pt-0" style=" background: #021b1e;">
+        <div class="col-12 d-flex justify-content-center align-items-center pt-0" style="background: #021b1e;">
             <h1 style="font-weight: bold; color: #10dd8e;">Análisis de Mercado Inteligente</h1>
         </div>
     </div>
@@ -30,16 +30,11 @@
                     <li>
                         <a href="{{ url('historyClient') }}" style="color: black;">Listados de Consultas</a>
                     </li>
-                    <b-collapse id="collapse-1">
-                        {{-- <li>
-              <a href="" style="color: black;"></a>
-              </li>
-              <li>
-              <a href="" style="color: black;"></a>
-              </li> --}}
-                    </b-collapse>
-                    <div class="d-flex justify-content-end"><a v-b-toggle.collapse-1 class="text-right"
-                            style="cursor: pointer; color: #10dd8e;">Ver más <i class="fas fa-angle-right"></i></a></div>
+                    <b-collapse id="collapse-1"></b-collapse>
+                    <div class="d-flex justify-content-end">
+                        <a v-b-toggle.collapse-1 class="text-right" style="cursor: pointer; color: #10dd8e;">Ver más <i
+                                class="fas fa-angle-right"></i></a>
+                    </div>
                 </ul>
             </div>
         </div>
@@ -53,17 +48,11 @@
                     <li>
                         <a href="{{ url('coupons-form') }}" style="color: black;">Consulta Diamond</a>
                     </li>
-
-                    <b-collapse id="collapse-2">
-                        {{-- <li>
-              <a href="{{ url('historyClient' )}}" style="color: black;"></a>
-              </li>
-              <li>
-              <a href="{{ url('historyClient' )}}" style="color: black;"></a>
-              </li>  --}}
-                    </b-collapse>
-                    <div class="d-flex justify-content-end"><a v-b-toggle.collapse-2 class="text-right"
-                            style="cursor: pointer; color: #10dd8e;">Ver más <i class="fas fa-angle-right"></i></a></div>
+                    <b-collapse id="collapse-2"></b-collapse>
+                    <div class="d-flex justify-content-end">
+                        <a v-b-toggle.collapse-2 class="text-right" style="cursor: pointer; color: #10dd8e;">Ver más <i
+                                class="fas fa-angle-right"></i></a>
+                    </div>
                 </ul>
             </div>
         </div>
@@ -77,17 +66,11 @@
                     <li>
                         <a href="{{ url('coupons-form') }}" style="color: black;">Consulta Diamond</a>
                     </li>
-
-                    <b-collapse id="collapse-3">
-                        {{-- <li>
-              <a href="" style="color: black;"></a>
-              </li>
-              <li>
-              <a href="" style="color: black;"></a>
-              </li>  --}}
-                    </b-collapse>
-                    <div class="d-flex justify-content-end"><a v-b-toggle.collapse-3 class="text-right"
-                            style="cursor: pointer; color: #10dd8e;">Ver más <i class="fas fa-angle-right"></i></a></div>
+                    <b-collapse id="collapse-3"></b-collapse>
+                    <div class="d-flex justify-content-end">
+                        <a v-b-toggle.collapse-3 class="text-right" style="cursor: pointer; color: #10dd8e;">Ver más <i
+                                class="fas fa-angle-right"></i></a>
+                    </div>
                 </ul>
             </div>
         </div>
@@ -99,14 +82,12 @@
                         <a href="" style="color: black;">Investigación de Bienes Comercial y Localización</a>
                     </li>
                     <li>
-                        <a href="{{ url('certificados') }}" style="color: black;">Certificados de Nacimiento -
-                            Defunción</a>
+                        <a href="{{ url('certificados') }}" style="color: black;">Certificados de Nacimiento - Defunción</a>
                     </li>
                     <li>
                         <a href="{{ url('demografico') }}" style="color: black;">Consultar masivamente datos demográficos
                             por documento</a>
                     </li>
-
                     <b-collapse id="collapse-4">
                         <li>
                             <a href="{{ url('historyClient') }}" style="color: black;">Datos Personales - Laborales</a>
@@ -115,8 +96,32 @@
                             <a href="{{ url('historyClient') }}" style="color: black;">Información Financiera</a>
                         </li>
                     </b-collapse>
-                    <div class="d-flex justify-content-end"><a v-b-toggle.collapse-4 class="text-right"
-                            style="cursor: pointer; color: #10dd8e;">Ver más <i class="fas fa-angle-right"></i></a></div>
+                    <div class="d-flex justify-content-end">
+                        <a v-b-toggle.collapse-4 class="text-right" style="cursor: pointer; color: #10dd8e;">Ver más <i
+                                class="fas fa-angle-right"></i></a>
+                    </div>
+                </ul>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 pt-5">
+            <div class="box-rounded bg-gainsboro text-black-pearl text-left">
+                <p class="box-label text-left"><span>LOCALIZACIÓN DE USUARIOS Y CARGA MASIVA DE PENSIONADOS</span></p>
+                <ul>
+                    <li>
+                        <a href="{{ url('colpensiones') }}" style="color: black;">Colpensiones</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('fiduprevisora') }}" style="color: black;">Fiduprevisora</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('joinpensiones') }}" style="color: black;">Localizar cedulas (subidas en
+                            archivo)</a>
+                    </li>
+                    <b-collapse id="collapse-5"></b-collapse>
+                    <div class="d-flex justify-content-end">
+                        <a v-b-toggle.collapse-5 class="text-right" style="cursor: pointer; color: #10dd8e;">Ver más <i
+                                class="fas fa-angle-right"></i></a>
+                    </div>
                 </ul>
             </div>
         </div>
