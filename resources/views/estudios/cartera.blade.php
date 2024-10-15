fc4851005906785b4a25b4489b0a2838e28b2a56
BrayanStyd11 <brayantriana22@gmail.com>
2024-10-15 16:11:54 -0500

    Configuración de vistas HEGO, pendiente vista nueva

--- resources/views/estudios/cartera.blade.php
+++ resources/views/estudios/cartera.blade.php
@@ -27,16 +27,16 @@ Cartera
                         <p class="pt-1 mb-0 font-weight-bold">Fechas:</p>
                     </div>
                     <div class="form-group col-md-1">
-                        <input type="text" class="form-control datepicker" name="filtro[fecha_desde]" id="fecha_desde" placeholder="Desde" value="{{ isset($filtro['fecha_desde']) ? $filtro['fecha_desde'] : '' }}">
+                        <input type="text" class="form-control2 datepicker" name="filtro[fecha_desde]" id="fecha_desde" placeholder="Desde" value="{{ isset($filtro['fecha_desde']) ? $filtro['fecha_desde'] : '' }}">
                     </div>
                     <div class="form-group">
                         <p class="pt-1 mb-0 font-weight-bold"> - </p>
                     </div>
                     <div class="form-group col-md-1">
-                        <input type="text" class="form-control datepicker" name="filtro[fecha_hasta]" id="fecha_hasta" placeholder="Hasta" value="{{ isset($filtro['fecha_hasta']) ? $filtro['fecha_hasta'] : '' }}">
+                        <input type="text" class="form-control2 datepicker" name="filtro[fecha_hasta]" id="fecha_hasta" placeholder="Hasta" value="{{ isset($filtro['fecha_hasta']) ? $filtro['fecha_hasta'] : '' }}">
                     </div>
                     <div class="form-group col-md-2 text-center">
-                        <select class="form-control" name="filtro[asesor]" id="asesor">
+                        <select class="form-control2" name="filtro[asesor]" id="asesor">
                             <option value="" <?php echo isset($filtro['asesor']) ? '' : ' selected'; ?>>Todos los asesores...</option>
                             @foreach (asesores() as $asesorgen)
                             <option value="{{ $asesorgen->id }}" <?php echo isset($filtro['asesor']) ? ($asesorgen->id == $filtro['asesor'] ? ' selected' : '') : ''; ?>>{{ $asesorgen->nombres }}
@@ -45,7 +45,7 @@ Cartera
                         </select>
                     </div>
                     <div class="form-group col-md-2 text-center">
-                        <select class="form-control" name="filtro[decision]" id="decision">
+                        <select class="form-control2" name="filtro[decision]" id="decision">
                             <option value="" <?php echo isset($filtro['decision']) ? '' : ' selected'; ?>>Todas las decisiones...</option>
                             @foreach (decisiones_estudios() as $key => $decision)
                             <option value="{{ $key }}" <?php echo isset($filtro['decision']) ? ($key == $filtro['decision'] ? ' selected' : '') : ''; ?>>{{ $decision }}
@@ -54,13 +54,13 @@ Cartera
                         </select>
                     </div>
                     <div class="form-group col-md-1">
-                        <input type="number" class="form-control datepicker-periodo" name="filtro[periodo]" id="periodo" placeholder="Periodo" value="{{ isset($filtro['periodo']) ? $filtro['periodo'] : '' }}">
+                        <input type="number" class="form-control2 datepicker-periodo" name="filtro[periodo]" id="periodo" placeholder="Periodo" value="{{ isset($filtro['periodo']) ? $filtro['periodo'] : '' }}">
                     </div>
                     <div class="form-group col-md-2">
-                        <input type="text" class="form-control" name="busq" id="busq" placeholder="Buscar por: Nombres - Documento" value="{{ isset($busq) ? $busq : '' }}">
+                        <input type="text" class="form-control2" name="busq" id="busq" placeholder="Buscar por: Nombres - Documento" value="{{ isset($busq) ? $busq : '' }}">
                     </div>
                     <div class="form-group col-md-1">
-                        <button type="submit" class="btn btn-info"><i class="fa fa-filter" aria-hidden="true"></i>
+                        <button style="padding: 8px 16px; border-radius: 8px; border-color: none; font-size: 14px; font-weight: 500; line-height: 18.23px; min-height: 40px; background-color: #0E866C; border-color: #0E866C; color: white;" type="submit" class="btn btn-info"><i class="fa fa-filter" aria-hidden="true"></i>
                             Filtrar</button>
                     </div>
                     @if (isset($busq) || isset($filtro))
@@ -73,7 +73,7 @@ Cartera
         </div>
         <div class="table-responsive">
             <table class="table table-hover table-striped table-condensed table-bordered">
-                <tr>
+                <tr class="thead-dark">
                     <th>C&eacute;dula</th>
                     <th>F. Desemb</th>
                     <th>Días rest.</th>
@@ -90,7 +90,7 @@ Cartera
                     <th>Cuotas<br>Pag.</th>
                     <th>Cuotas<br>Causad.</th>
                     <th>Cuotas<br>Mora</th>
-                    <th><img src="../images/planpagos.png" title="Plan de Pagos"></th>
+                    <th>Plan de Pagos</th>
                 </tr>
                 <tbody>
                     @foreach ($lista as $cartera)
@@ -112,8 +112,10 @@ Cartera
                         <td>0</td>
                         <td>0</td>
                         <td class="text-center">
-                            <a href="#" title="Modificar" class="btn btn-info"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                            <a onclick="return confirm('Seguro que desea eliminar este registro y su informacion relacionada?')" href="#" title="Eliminar" class="btn btn-info"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                            <div class="row">
+                                <a class="btn btn-sm" style="color:green" href="#" title="Modificar"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                                <a class="btn btn-sm" style="color:green" onclick="return confirm('Seguro que desea eliminar este registro y su informacion relacionada?')" href="#" title="Eliminar"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                            </div>
                         </td>
                     </tr>
                     @endforeach
