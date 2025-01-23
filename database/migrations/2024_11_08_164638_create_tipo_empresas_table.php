19429329f44bfd12d62e700371dbe52954d4b6a7
Jhon Michael Garcia <me@maikoldev.com>
2025-01-22 22:10:09 -0500

    Fix Migrations

--- database/migrations/2024_11_08_164638_create_tipo_empresas_table.php
+++ database/migrations/2024_11_08_164638_create_tipo_empresas_table.php
@@ -6,28 +6,28 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTipoEmpresasTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('tipo_empresas', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('nombre');
-			$table->timestamps();
-			$table->softDeletes();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('tipo_empresas');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_empresas');
+    }
 }
